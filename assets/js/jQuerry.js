1ebc13c15b09f1636c060950fbe5cf9b894236aa
RafliMagangs <raflirangga35@gmail.com>
2024-10-22 15:50:57 +0700

    Add Meet Our Teams and Edit Nav-item Locations

--- assets/js/jQuerry.js
+++ assets/js/jQuerry.js
@@ -280,3 +280,17 @@ $(document).ready(function () {
     renderCourses(filteredCourses);
   });
 });
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-speakers").owlCarousel({
+      autoPlay: 6000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
